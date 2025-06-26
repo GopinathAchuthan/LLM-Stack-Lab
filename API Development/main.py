bae3a8617dd243dc7b78f6d62b8168e4d146efb9
Gopinath Achuthan <agopinathus@gmail.com>
2025-06-25 23:07:01 -0500

    Testing fastAPI

--- API Development/main.py
+++ API Development/main.py
@@ -1,1 +1,18 @@
-print('Hello World')
+from fastapi import FastAPI
+from fastapi import Body
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to My API"}
+
+@app.get("/posts")
+async def get_posts():
+    return {'data': 'This is your posts'}
+
+@app.post("/create_post")
+async def create_post(payload: dict = Body(...)):
+    return {"data": payload}
+    # return {"message": "successfully created post"}
