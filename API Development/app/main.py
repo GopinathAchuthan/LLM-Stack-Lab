abe8dc2bb9f2981d48eec01edea633b00201b4ba
Gopinath Achuthan <agopinathus@gmail.com>
2025-07-03 16:06:35 -0500

    creating users table

--- API Development/app/main.py
+++ API Development/app/main.py
@@ -1,11 +1,11 @@
-from typing import Optional
-from fastapi import Depends, FastAPI, Response, status, HTTPException
-from fastapi import Body
-from . import models, schemas
-from .database import engine, get_db
-import psycopg
-import time
+from typing import List
+from fastapi import FastAPI, Response, status, HTTPException
+from fastapi import Depends
+from app import models, schemas
+from app.database import engine, get_db
 from sqlalchemy.orm import Session
+from app.utils import hash_password
+
 
 app = FastAPI()
 
@@ -16,32 +16,27 @@ models.Base.metadata.create_all(bind=engine)
 async def root():
     return {"message": "Welcome to My API"}
 
-@app.get("/sqlalchemy")
-async def test_db_connection(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-    return {"data": posts}
-
-@app.get("/posts")
+@app.get("/posts", response_model=List[schemas.PostResponse])
 async def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
-    return {'data': posts}
+    return posts
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
 async def create_post(post: schemas.Posts, db: Session = Depends(get_db)):
     new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-    return {"data": new_post}
+    return new_post
 
 
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model=schemas.PostResponse)
 def get_posts(id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id ==id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"Post {id} not found")
-    return {"Post": post}
+    return post
 
 @app.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_posts(id: int, db: Session = Depends(get_db)):
@@ -66,3 +61,18 @@ async def update_post(id: int, post: schemas.Posts, db: Session = Depends(get_db
     db.commit()
     update_post = post_query.first()
     return update_post
+
+
+
+@app.post("/users", response_model=schemas.UserResponse)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    hashed_pwd = hash_password(user.password)
+    new_user = models.User(
+        name=user.name,
+        email=user.email,
+        password=hashed_pwd
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
--- API Development/app/schemas.py
+++ API Development/app/schemas.py
@@ -1,4 +1,6 @@
 from pydantic import BaseModel, ConfigDict
+from pydantic import EmailStr
+from datetime import datetime
 
 class Posts(BaseModel):
     title: str
@@ -7,4 +9,19 @@ class Posts(BaseModel):
 
 class PostResponse(Posts):
     id: int
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
+
+class UserCreate(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: EmailStr
+    created_at: datetime
+
     model_config = ConfigDict(from_attributes=True)
